2182ef4fa00f05bcc068b0eb5be84bfec090c1a2
Aurélien Plazzotta <aurelien.plazzotta@tutanota.com>
2025-01-15 15:56:12 +0100

    Added first functionality

---
+++ src/main/java/Dinosaur.java
@@ -0,0 +1,11 @@
+public class Dinosaur {
+    private String name;
+
+    public Dinosaur(String name) {
+        this.name = name;
+    }
+
+    public void greet() {
+        System.out.println("Hi, I am " + this.name + "!");
+    }
+}
--- src/main/java/EdenAssistant.java
+++ src/main/java/EdenAssistant.java
@@ -13,7 +13,41 @@ public class EdenAssistant {
         while (true) {
             displayMenu();
             byte choiceItemMenu = scanner.nextByte();
-            handleChoiceMenu();
+            handleChoiceMenu(choiceItemMenu);
+        }
+    }
+
+    public void displayMenu() {
+        System.out.println();
+        System.out.println("""
+                Welcome to Mesozoic Eden assistant!
+                1. Add dinosaur
+                2. Check park hours
+                3. Greet guest
+                4. Check visitors count
+                5. Manage staff
+                6. Exit
+                """);
+        System.out.print("Enter your choice: ");
+    }
+
+    public void addDinosaur() {
+        Dinosaur dino = new Dinosaur("paco");
+        dino.greet();
+    }
+
+    public void handleChoiceMenu(byte choice) {
+        switch (choice) {
+            case 1 -> addDinosaur();
+//            case 2 -> checkParkHours();
+//            case 3 -> greetGuest();
+//            case 4 -> checkVisitorsCount();
+//            case 5 -> manageStaff();
+            case 6 -> {
+                System.out.println("Exiting...");
+                System.exit(0);
+            }
+            default -> System.exit(0);
         }
     }
 }
